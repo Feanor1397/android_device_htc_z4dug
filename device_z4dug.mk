745064a94e523720bdc5a634555aa6afe03ba72d
Feanor1397 <feanor1397@gmail.com>
2016-09-13 20:36:03 +0400

    and again dat ramdisk\(

--- device_z4dug.mk
+++ device_z4dug.mk
@@ -34,10 +34,15 @@ PRODUCT_BOOT_JARS += qcmediaplayer
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
+    device/htc/z4dug/ramdisk/init:root/init \
+    device/htc/z4dug/ramdisk/init.rc:root/init.rc \
     device/htc/z4dug/ramdisk/init.usb.rc:root/init.usb.rc \
     device/htc/z4dug/ramdisk/init.target.rc:root/init.target.rc \
+    device/htc/z4dug/ramdisk/init.trace.rc:root/init.trace.rc \
+    device/htc/z4dug/ramdisk/init.cm.rc:root/init.cm.rc \
     device/htc/z4dug/ramdisk/init.qcom.rc:root/init.qcom.rc \
     device/htc/z4dug/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/htc/z4dug/ramdisk/ueventd.rc:root/ueventd.rc \
     device/htc/z4dug/ramdisk/ueventd.target.rc:root/ueventd.target.rc \
     device/htc/z4dug/ramdisk/default.prop:root/default.prop
 
